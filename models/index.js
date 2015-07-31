ac12e7a309d95382d5fca1b0ec9decb75247770c
shmublon <ana@3rhia>
2015-07-31 17:53:03 +0300

    change DB for postgreSQL

--- models/index.js
+++ models/index.js
@@ -6,10 +6,10 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize("generatordb", "root", "",
+var sequelize = new Sequelize("generatordb", "postgres", "postgres",
     {
-        dialect: "mysql",
-        port:    3306
+        dialect: "postgres",
+        port:    5432
     });
 var db = {};
 
