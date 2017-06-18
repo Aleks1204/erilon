68334c894435240753baa9f6a1c185478a09d634
shmublon <shmublon@gmail.com>
2017-06-18 15:11:27 +0200

    похуйчто

--- routes/image-api.js
+++ routes/image-api.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var cloudinary = require('cloudinary');
+
+cloudinary.config({
+    cloud_name: 'hjzqny4m9',
+    api_key: '934772951151964',
+    api_secret: 'SYTVrZ2QvK3f_zEVbejHzM2wVAk'
+});
 
 router.post('/uploadAvatar/:personage_id', function (req, res) {
     var file = req.body.file;
@@ -8,7 +15,11 @@ router.post('/uploadAvatar/:personage_id', function (req, res) {
     var data = file.substring(file.indexOf(',') + 1);
     var buf = new Buffer(data, 'base64');
     fs.writeFile(newPath, buf);
-    return res.send({status:'DONE'});
+    var public_id = req.body.name.replace(".png", "");
+    console.log('id: '+ public_id);
+    cloudinary.uploader.upload(newPath.replace('[.]png', ''), function(result) {
+        return res.send({status:'DONE'});
+    }, { public_id:  public_id});
 });
 
 module.exports = router;
