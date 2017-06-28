fc001b6acc9233094637eab6b6b7a7c1f239ba19
shmublon <shmublon@gmail.com>
2017-06-28 17:38:53 +0200

    похуйчто

--- public/views/profile/profile.js
+++ public/views/profile/profile.js
@@ -23,6 +23,64 @@ app.controller("profileController", function ($scope, $http, $q, $localStorage) 
         });
     });
 
+    $scope.uploadPlayerAvatar = function (player_id) {
+        var file = '';
+        var date = new Date();
+        var name = 'player_avatar_' + player_id + '_' + date.getTime() + '.png';
+        swal({
+            title: 'Загрузить аватар игрока',
+            input: 'file',
+            inputAttributes: {
+                accept: 'image/*'
+            },
+            inputClass: 'dropify',
+            onOpen: function () {
+                var input = $('.swal2-file.dropify');
+                input.attr('data-max-file-size', '3M');
+                input.dropify({
+                    messages: {
+                        'default': 'Перетащите картинку в область или кликните для загрузки',
+                        'replace': 'Перетащите картинку в область или кликните для загрузки',
+                        'remove': 'Удалить',
+                        'error': 'Уппссс... что то пошло не так'
+                    },
+                    error: {
+                        'fileSize': 'Нельзя загружать аватар больше {{ value }}.',
+                        'minWidth': 'The image width is too small ({{ value }}}px min).',
+                        'maxWidth': 'The image width is too big ({{ value }}}px max).',
+                        'minHeight': 'The image height is too small ({{ value }}}px min).',
+                        'maxHeight': 'The image height is too big ({{ value }}px max).',
+                        'imageFormat': 'The image format is not allowed ({{ value }} only).'
+                    }
+                });
+                file = $('.dropify-render img').attr('src');
+            },
+            showLoaderOnConfirm: true,
+            allowOutsideClick: false,
+            preConfirm: function () {
+                return new Promise(function (resolve) {
+                    file = $('.dropify-render img').attr('src');
+                    if (file !== undefined) {
+                        $http.put('/players/' + player_id, {
+                            avatar: name
+                        }).then(function () {
+                            $http.post('/uploadPlayerAvatar/' + player_id, {
+                                file: file,
+                                name: name
+                            }).then(function () {
+                                $http.get('/players/' + $localStorage.playerId).then(function (response) {
+                                    $scope.player = response.data.player;
+                                });
+                            });
+                        });
+                    } else {
+                        resolve();
+                    }
+                })
+            }
+        });
+    };
+
     // $scope.setInitialPositionsForAttributes = function () {
     //     var attributesDefer = $q.defer();
     //     var playersDefer = $q.defer();
